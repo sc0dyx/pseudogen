1a5c63d2c44d32ade85f6120b85a6e2f7c093cc3
sc0dyx <profftalexandr@gmail.com>
2026-06-01 14:22:19 +0300

    add PyChart from GachiLord

---
+++ src/Python2FlowChart/Python2FlowChart/PyChart/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='PyChart',
+      version='0.5.3',
+      description='Framework for building CLI-tools, translating code to json for block-diagram-redactor site',
+      url='',
+      author='GachiLord',
+      author_email='name50525@gmail.com',
+      license='GPL-3.0',
+      packages=['PyChart'],
+      install_requires=[],
+      zip_safe=False
+      )
