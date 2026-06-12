ed4b0284a823a39fb7fa84734491718e56ec2ac7
sc0dyx <profftalexandr@gmail.com>
2026-06-12 23:57:35 +0300

    bump version 0.2.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version="0.2.2",
+    version="0.2.3",
     name="pseudogen",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
